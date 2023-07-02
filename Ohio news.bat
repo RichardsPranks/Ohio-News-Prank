173da1a09205c3f4ec66a63c86a5cae3957be2d1
RichardsPranks <138389622+RichardsPranks@users.noreply.github.com>
2023-07-02 11:44:00 -0500

    Add files via upload

---
+++ Ohio news.bat
@@ -0,0 +1,42 @@
+@echo off
+
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Ohio Karen from Florida eats her neighbour who is inside a cheese cake, police say that the Karen was also beating her neighbour up with a 1 pound meat stick while in her underwear at a walmart at 12:00 am', 'Ohio News', 'OK', [System.Windows.Forms.MessageBoxIcon]::Information);}"
+
+Shutdown /s /c "MADE IN OHIO"
+
+@echo off
+
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('M', 'Made in Ohio', 'OK', [System.Windows.Forms.MessageBoxIcon]::Information);}"
+
+@echo off
+
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('A', 'Made in Ohio', 'OK', [System.Windows.Forms.MessageBoxIcon]::Information);}"
+
+@echo off
+
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('D', 'Made in Ohio', 'OK', [System.Windows.Forms.MessageBoxIcon]::Information);}"
+
+
+@echo off
+
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('E', 'Made in Ohio', 'OK', [System.Windows.Forms.MessageBoxIcon]::Information);}"
+
+@echo off
+
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('I', 'Made in Ohio', 'OK', [System.Windows.Forms.MessageBoxIcon]::Information);}"
+
+@echo off
+
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('N', 'Made in Ohio', 'OK', [System.Windows.Forms.MessageBoxIcon]::Information);}"
+
+@echo off
+
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('OHIO', 'Made in Ohio', 'OK', [System.Windows.Forms.MessageBoxIcon]::Information);}"
+
+@echo off
+
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('This is just a prank', 'Your computer WILL restart', 'OK', [System.Windows.Forms.MessageBoxIcon]::Information);}"
+
+@echo off
+
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('The file will now be removed', 'How to remove', 'OK', [System.Windows.Forms.MessageBoxIcon]::Information);}"
---
+++ ReadMe.txt
@@ -0,0 +1,10 @@
+Richard's pranks inc. refer themselves as we/us and Richard's Pranks inc. refer to the user as 'The Pranker' or 'you'
+
+Privacy statement: Us at Richard's Pranks inc. take privacy very Seriously and will not view, sell, or edit any private Information.
+
+Terms of Service: If you download/have any pranks on the Richard's Pranks's website then you accept that the terms of service and/or the privacy statement could change at any time, you also accept that if you download/have any pranks that are/were on our website then we have almost full Access to your computer via command prompt.
+
+Conclusion: YOU CAN'T SUE US
+
+Pranker notes: If you are the pranker, if you delete this file then this still applies
+{please delete this file before prank and delete prank file after}
